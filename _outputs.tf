8da993bdaa3c4db71c31b6ab02743184a62e31a3
Evandro Myller <emyller@d7.dev>
2022-02-23 12:40:24 -0300

    Adjust outputs

--- _outputs.tf
+++ _outputs.tf
@@ -1,9 +1,9 @@
-output "key_id" {
+output "access_key_id" {
   description = "The user's AWS access key ID."
   value = module.user.iam_access_key_id
 }
 
-output "secret_key" {
+output "access_key_secret" {
   description = "The AWS secret access key of the user's key."
   value = module.user.iam_access_key_secret
 }
