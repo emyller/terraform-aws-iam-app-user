6f5ff419a5a2f5694562a5778d10b5c6204f9970
Evandro Myller <emyller@d7.dev>
2022-03-29 00:21:26 -0300

    Fix compatibility break in external module

--- user.tf
+++ user.tf
@@ -1,6 +1,6 @@
 module "user" {
   source = "terraform-aws-modules/iam/aws//modules/iam-user"
-  version = "~> 4.0"
+  version = "~> 4.16.0"
 
   name = var.name
   create_iam_user_login_profile = false
