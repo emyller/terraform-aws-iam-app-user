1b050c2d9bbf0bed86db1b8b3cd504b02cb1369a
Evandro Myller <emyller@d7.dev>
2022-08-28 19:59:42 -0300

    Output the user ARN

--- _outputs.tf
+++ _outputs.tf
@@ -7,3 +7,8 @@ output "access_key_secret" {
   description = "The AWS secret access key of the user's key."
   value = module.user.iam_access_key_secret
 }
+
+output "arn" {
+  description = "ARN of the IAM user."
+  value = module.user.iam_user_arn
+}
